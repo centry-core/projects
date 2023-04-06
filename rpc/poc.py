65906722c2942aeef3a647ba7c3fb99b08efc435
Dmitrii_Belov1 <Dmitrii_Belov1@epam.com>
2023-04-06 12:52:01 +0600

    Prototype of roles

--- rpc/poc.py
+++ rpc/poc.py
@@ -9,19 +9,17 @@ class RPC:
     @rpc_tools.wrap_exceptions(RuntimeError)
     def list_user_projects(self, user_id, **kwargs):
         all_projects = self.list(**kwargs)
-        root_permissions = auth.get_user_permissions(user_id, 1)
-        scope_map = {item["name"]:item["id"] for item in auth.list_scopes()}
         #
+        log.info(f"projects {user_id=} {all_projects=}")
         user_projects = list()
-        #
         for project in all_projects:
-            project_scope_id = scope_map.get(f"Project-{project['id']}", 1)
-            project_permissions = auth.get_user_permissions(user_id, project_scope_id)
+            project_users = self.context.rpc_manager.call.get_users_in_project(
+                project["id"])
             #
-            log.info("Project scope id: %s", project_scope_id)
-            log.info("Project permissions: %s", project_permissions)
+            log.info("Project users: %s", project_users)
             #
-            if "global_admin" in root_permissions or "project_member" in project_permissions or 'project_admin' in project_permissions:
+            if user_id in {user["auth_id"] for user in project_users}:
                 user_projects.append(project)
+
         #
         return user_projects
