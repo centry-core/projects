f7a440e6e262ce54bd55f1233f82263e5f7225a0
Aspect13 <ad13box@gmail.com>
2023-04-27 18:54:09 +0400

    splitting project create process

--- api/v1/project.py
+++ api/v1/project.py
@@ -1,23 +1,17 @@
-from datetime import datetime
 from typing import Optional, Tuple
 from flask import request, g
 from pylon.core.tools import log
 
 from pydantic import ValidationError
-from sqlalchemy import schema
 
 from tools import auth, VaultClient, TaskManager, db, api_tools, db_tools
 
 from ...models.pd.project import ProjectCreatePD
 from ...models.project import Project
-from ...models.statistics import Statistic
-from ...models.quota import ProjectQuota
-from ...utils import create_project_influx_databases, drop_project_influx_databases, generate_project_secrets, \
-    add_project_token, create_project_user
-from ...tools.rabbit_tools import create_project_user_and_vhost
-from ...tools.session_plugins import SessionProjectPlugin
 
-PROJECT_ROLE_NAME = 'default'
+from ...utils import drop_project_influx_databases, create_project_token, create_project_user, create_project_model, \
+    create_project_schema, create_project_secrets
+from ...tools.session_plugins import SessionProjectPlugin
 
 
 class ProjectAPI(api_tools.APIModeHandler):
@@ -75,51 +69,14 @@ class AdminAPI(api_tools.APIModeHandler):
         except ValidationError as e:
             return e.errors(), 400
 
-        # with db_tools.session.begin():
-        # sp = db_tools.session.begin_nested()
-        def create_project_model(pd_model: ProjectCreatePD, owner_id: int) -> Project:
-            project = Project(
-                name=pd_model.name,
-                plugins=pd_model.plugins,
-                owner_id=owner_id
-            )
-            project.insert()
-            log.info('after project.insert()')
-            ProjectQuota.create(
-                project_id=project.id,
-                vuh_limit=pd_model.vuh_limit,
-                storage_space=pd_model.storage_space_limit,
-                data_retention_limit=pd_model.data_retention_limit
-            )
-            log.info('after quota created')
-
-            statistic = Statistic(
-                project_id=project.id,
-                start_time=str(datetime.utcnow()),
-            )
-            statistic.insert()
-            log.info('after statistic created')
-            return project
-
         project = create_project_model(project_model, g.auth.id)
-        # pid = str(project.id)
-
-        # sp.rollback()
-        # return f'Project {pid} should not be in db', 400
 
         SessionProjectPlugin.set(project.plugins)  # this looks bad
 
-        def create_project_schema(project_id: int) -> None:
-            with db.with_project_schema_session(project_id) as tenant_db:
-                tenant_db.execute(schema.CreateSchema(f"Project-{project_id}"))
-                db.get_tenant_specific_metadata().create_all(bind=tenant_db.connection())
-                tenant_db.commit()
-            log.info("Project schema created")
-
         # Create project schema
         create_project_schema(project.id)
-        project_roles = auth.get_roles(mode=PROJECT_ROLE_NAME)
-        project_permissions = auth.get_permissions(mode=PROJECT_ROLE_NAME)
+        project_roles = auth.get_roles(mode='default')
+        project_permissions = auth.get_permissions(mode='default')
         log.info('after permissions received')
 
         for role in project_roles:
@@ -132,21 +89,7 @@ class AdminAPI(api_tools.APIModeHandler):
             )
         log.info('after permissions set for roles')
 
-        # #
-        # # Auth: create project scope
-        # #
-        # scope_map = {item["name"]: item["id"] for item in auth.list_scopes()}
-        # scope_name = f"Project-{project.id}"
-        # #
-        # if scope_name not in scope_map:
-        #     scope_id = auth.add_scope(scope_name, parent_id=1)
-        #     log.info("Created project scope: %s -> %s", scope_name, scope_id)
-        # else:
-        #     scope_id = scope_map[scope_name]
-
-        #
         # Auth: create project admin
-        #
         log.info('adding project admin')
         ROLES = ['admin', ]
         self.module.add_user_to_project_or_create(
@@ -156,7 +99,7 @@ class AdminAPI(api_tools.APIModeHandler):
             roles=ROLES
         )
 
-        vault_client = VaultClient.from_project(project.id)
+        vault_client = VaultClient.from_project(project)
         try:
             project_vault_data = vault_client.init_project_space()
         except:
@@ -170,27 +113,15 @@ class AdminAPI(api_tools.APIModeHandler):
             "vault_auth_role_id": project_vault_data["auth_role_id"],
             "vault_auth_secret_id": project_vault_data["auth_secret_id"],
         }
-        # project.worker_pool_config_json = {
-        #     "regions": ["default"]
-        # }
         project.commit()
         log.info('after project secrets_json set')
 
         user_id = create_project_user(project_id=project.id)
         log.info('after project tech user is created')
-        token = add_project_token(user_id)
+        token = create_project_token(user_id)
         log.info('after tech token issued')
-        project_secrets, project_hidden_secrets = generate_project_secrets(project.id)
-        project_secrets["auth_token"] = token
 
-        vault_client.set_project_secrets(project_secrets)
-        log.info('after set_project_secrets')
-        vault_client.set_project_hidden_secrets(project_hidden_secrets)
-        log.info('after set_project_hidden_secrets')
-        create_project_user_and_vhost(project.id)
-        log.info('after create_project_user_and_vhost')
-        create_project_influx_databases(project.id)
-        log.info('after create_project_influx_databases')
+        create_project_secrets(vault_client, token)
 
         self.module.context.rpc_manager.timeout(3).check_rabbit_queues()
         log.info('after run rabbit task')
@@ -203,7 +134,6 @@ class AdminAPI(api_tools.APIModeHandler):
                 'one_recipient': project_model.project_admin_email,
                 'one_role': ROLES[0],
                 'subject': 'Invitation to a Centry project',
-                'debug_sleep': '1'
             }], project_model.invitation_integration)
 
         return project.to_json(exclude_fields=Project.API_EXCLUDE_FIELDS), 201
--- constants.py
+++ constants.py
@@ -4,3 +4,5 @@ INFLUX_DATABASES = {
     'comparison_db': 'comparison_{}',
     'telegraf_db': 'telegraf_{}'
 }
+PROJECT_USER_NAME_TEMPLATE = ':Carrier:Project:{}:'
+PROJECT_USER_EMAIL_TEMPLATE = '{}@system.project.user'
