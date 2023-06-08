b4a9cf8ee68b9d10b34951f872ac15c4fe6e257f
Aspect13 <ad13box@gmail.com>
2023-06-08 15:18:18 +0200

    secrets - make some secrets persistent tasks - result record is created on centry side + sanitizing interceptor logs

--- utils/__init__.py
+++ utils/__init__.py
@@ -12,7 +12,6 @@ def get_project_user(project_id: int) -> Optional[dict]:
 
 def generate_project_secrets(project_id: int) -> Tuple[dict, dict]:
     project_secrets = {
-        'galloper_url': c.APP_HOST,
         'project_id': project_id,
     }
 
