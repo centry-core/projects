a7f3c9da2d851e2548b8d50a660c5f1d1e68cc64
Aspect13 <ad13box@gmail.com>
2023-12-06 12:26:54 +0400

    constants fix

--- constants.py
+++ constants.py
@@ -4,9 +4,9 @@ INFLUX_DATABASES = {
     'comparison_db': 'comparison_{}',
     'telegraf_db': 'telegraf_{}'
 }
-PROJECT_USER_NAME_TEMPLATE = ':System:Project:{}:'
-PROJECT_USER_EMAIL_TEMPLATE = 'system_p_{}@centry.user'
-PROJECT_SCHEMA_TEMPLATE = 'P_{}'
+PROJECT_USER_NAME_TEMPLATE = ':system:project:{}:'
+PROJECT_USER_EMAIL_TEMPLATE = 'system_user_{}@centry.user'
+PROJECT_SCHEMA_TEMPLATE = 'p_{}'
 PROJECT_RABBIT_USER_TEMPLATE = 'rabbit_user_{}'
 PROJECT_RABBIT_VHOST_TEMPLATE = 'project_{}_vhost'
-PROJECT_PERSONAL_NAME_TEMPLATE = 'Project_User_{user_id}'
+PROJECT_PERSONAL_NAME_TEMPLATE = 'project_user_{user_id}'
